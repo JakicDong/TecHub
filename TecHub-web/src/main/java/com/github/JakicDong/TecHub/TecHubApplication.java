07b0140ec0328d1ce62d842680dfd8d1ef444a92
Hanya44 <2062966830@qq.com>
2025-06-25 14:47:44 +0800

    TecHub初始化

---
+++ TecHub-web/src/main/java/com/github/JakicDong/TecHub/TecHubApplication.java
@@ -0,0 +1,15 @@
+package com.github.JakicDong.TecHub;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JakicDong
+ * @time 2025.6.25
+ * @description 启动类
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class TecHubApplication {
+    //这是一个入口类
+
+}
