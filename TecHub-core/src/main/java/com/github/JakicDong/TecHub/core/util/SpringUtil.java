56e098de67e23e0c2f91f1d8d469a27d656fd2a8
JakicDong <2062966830@qq.com>
2025-06-27 16:26:49 +0800

    实现了数据库表的自动初始化,使用liquibase管理表更改,并且实现了在不启用liquibase的时候的初始化器.

---
+++ TecHub-core/src/main/java/com/github/JakicDong/TecHub/core/util/SpringUtil.java
@@ -0,0 +1,102 @@
+package com.github.JakicDong.TecHub.core.util;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author JakicDong
+ * @time 2025.6.27
+ * @description Spring工具类
+ * @version 1.0.0
+ */
+@Component
+public class SpringUtil implements ApplicationContextAware, EnvironmentAware {
+    //让 Bean 能获取到 Spring 容器的上下文（ApplicationContext）
+    //让 Bean 能获取到环境配置（Environment）
+    //volatile可以使得当一个线程修改volatile变量时，其他线程能立即看到最新
+    private volatile static ApplicationContext context;
+    private volatile static Environment environment;
+
+    private static Binder binder;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringUtil.context = applicationContext;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        SpringUtil.environment = environment;
+        binder = Binder.get(environment);
+    }
+
+    public static ApplicationContext getContext() {
+        return context;
+    }
+
+    /**
+     * 获取bean
+     *
+     * @param bean
+     * @param <T>
+     * @return
+     */
+    public static <T> T getBean(Class<T> bean) {
+        return context.getBean(bean);
+    }
+
+    public static <T> T getBeanOrNull(Class<T> bean) {
+        try {
+            return context.getBean(bean);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 获取配置
+     *
+     * @param key
+     * @return
+     */
+    public static String getConfig(String key) {
+        return environment.getProperty(key);
+    }
+
+    public static String getConfigOrElse(String mainKey, String slaveKey) {
+        String ans = environment.getProperty(mainKey);
+        if (ans == null) {
+            return environment.getProperty(slaveKey);
+        }
+        return ans;
+    }
+
+    /**
+     * 发布事件消息
+     *
+     * @param event
+     */
+    public static void publishEvent(ApplicationEvent event) {
+        context.publishEvent(event);
+    }
+
+
+    /**
+     * 配置绑定类
+     *
+     * @return
+     */
+    public static Binder getBinder() {
+        return binder;
+    }
+
+
+}
