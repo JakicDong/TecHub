7b2edcb6398b69bd59bd1f56d6ccf5856fcdb182
JakicDong <2062966830@qq.com>
2025-07-08 18:35:14 +0800

    实现日志过滤器中

---
+++ techub-web/src/main/java/com/github/jakicdong/techub/web/front/login/pwd/LoginRestController.java
@@ -0,0 +1,11 @@
+package com.github.jakicdong.techub.web.front.login.pwd;
+
+/*
+* @author JakicDong
+* @description 用户名 密码方式的登录/登出的入口
+* @time 2025/7/8 14:25
+*/
+public class LoginRestController {
+    
+
+}
--- techub-web/src/main/java/com/github/jakicdong/techub/web/hook/filter/ReqRecordFilter.java
+++ techub-web/src/main/java/com/github/jakicdong/techub/web/hook/filter/ReqRecordFilter.java
@@ -1,7 +1,21 @@
 package com.github.jakicdong.techub.web.hook.filter;
 
-public class ReqRecordFilter {
-    //todo
+import lombok.extern.slf4j.Slf4j;
 
-}
+import javax.servlet.annotation.WebFilter;
+
+/*
+* @author JakicDong
+* @description ReqRecord的过滤器
+*  1. 请求参数日志输出过滤器
+*  2. 判断用户是否登录
+* @time 2025/7/8 14:10
+*/
+//@Slf4j
+//@WebFilter(urlPatterns = "/*", filterName = "reqRecordFilter", asyncSupported = true)
+//public class ReqRecordFilter {
+//
+//
+//
+//}
 
