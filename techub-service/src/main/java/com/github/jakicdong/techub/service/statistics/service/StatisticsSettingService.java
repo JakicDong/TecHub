3b188ffdef652f6cb91c4262be8ed0396472928e
JakicDong <2062966830@qq.com>
2025-07-11 17:04:49 +0800

    正在实现ReqRecordFilter过滤器,有报错

---
+++ techub-service/src/main/java/com/github/jakicdong/techub/service/statistics/service/StatisticsSettingService.java
@@ -0,0 +1,11 @@
+package com.github.jakicdong.techub.service.statistics.service;
+
+
+/*
+* @author JakicDong
+* @description 数据统计后台接口
+* @time 2025/7/11 17:04
+*/
+
+public interface StatisticsSettingService {
+}
