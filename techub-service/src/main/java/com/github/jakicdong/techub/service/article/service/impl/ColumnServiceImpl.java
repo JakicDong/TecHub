a9c6d443a2c8d1f28201e5edef838d9a79d2c780
JakicDong <2062966830@qq.com>
2025-07-12 09:28:11 +0800

    实现ReqRecordFilter过滤器

---
+++ techub-service/src/main/java/com/github/jakicdong/techub/service/article/service/impl/ColumnServiceImpl.java
@@ -0,0 +1,10 @@
+package com.github.jakicdong.techub.service.article.service.impl;
+
+import com.github.jakicdong.techub.service.article.service.ColumnService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ColumnServiceImpl implements ColumnService {
+}
