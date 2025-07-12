2e262a5364a1f93cf30c0311cf39651d08473a38
JakicDong <2062966830@qq.com>
2025-07-12 14:49:05 +0800

    正在实现ReqRecordFilter过滤器,实现了CORS跨域支持类

---
+++ techub-core/src/main/java/com/github/jakicdong/techub/core/mdc/MdcUtil.java
@@ -0,0 +1,36 @@
+package com.github.jakicdong.techub.core.mdc;
+
+import org.slf4j.MDC;
+
+/*
+* @author JakicDong
+* @description 映射诊断上下文
+* @time 2025/7/12 14:39
+*/
+public class MdcUtil {
+
+    public static final String TRACE_ID_KEY = "traceId";
+
+    public static void add(String key, String val) {
+        MDC.put(key, val);
+    }
+
+    public static void addTraceId() {
+        // traceId的生成规则，技术派提供了两种生成策略，可以使用自定义的也可以使用SkyWalking; 实际项目中选择一种即可
+        MDC.put(TRACE_ID_KEY, SelfTraceIdGenerator.generate());
+    }
+
+    public static String getTraceId() {
+        return MDC.get(TRACE_ID_KEY);
+    }
+
+    public static void reset() {
+        String traceId = MDC.get(TRACE_ID_KEY);
+        MDC.clear();
+        MDC.put(TRACE_ID_KEY, traceId);
+    }
+
+    public static void clear() {
+        MDC.clear();
+    }
+}
