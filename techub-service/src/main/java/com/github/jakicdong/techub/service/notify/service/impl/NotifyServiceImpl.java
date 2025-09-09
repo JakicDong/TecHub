df282df7ccc9d2b7a808ce605ed2b88114a01381
JakicDong <2062966830@qq.com>
2025-09-09 09:46:45 +0800

    debug:wsUserSessionCache未初始化导致出现NPE

--- techub-service/src/main/java/com/github/jakicdong/techub/service/notify/service/impl/NotifyServiceImpl.java
+++ techub-service/src/main/java/com/github/jakicdong/techub/service/notify/service/impl/NotifyServiceImpl.java
@@ -5,14 +5,19 @@ import com.github.jakicdong.techub.api.model.enums.NotifyStatEnum;
 import com.github.jakicdong.techub.core.ws.WebSocketResponseUtil;
 import com.github.jakicdong.techub.service.notify.repository.dao.NotifyMsgDao;
 import com.github.jakicdong.techub.service.notify.service.NotifyService;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -27,7 +32,18 @@ public class NotifyServiceImpl implements NotifyService {
      */
     private LoadingCache<Long, Set<String>> wsUserSessionCache;
 
-
+    @PostConstruct
+    public void init() {
+        wsUserSessionCache = CacheBuilder.newBuilder()
+                .maximumSize(500)
+                .expireAfterAccess(1, TimeUnit.HOURS)
+                .build(new CacheLoader<Long, Set<String>>() {
+                    @Override
+                    public Set<String> load(Long aLong) throws Exception {
+                        return new HashSet<>();
+                    }
+                });
+    }
 
     @Override
     public int queryUserNotifyMsgCount(Long userId) {
